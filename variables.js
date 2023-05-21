9e10ceac3b897ab978b453322bf4bdeaa131b0a3
Carlos Andrés Agudelo <kharlz.agudelo@hotmail.com>
2023-05-21 17:00:49 -0500

    Creando archivos de valores y variables

---
+++ variables.js
@@ -0,0 +1,17 @@
+var nombre = "Carlos";
+
+// Declarar
+var edad;
+// Inicializar
+edad = 25;
+
+edad;
+
+var elementos = ["PC", "Mobile"];
+
+var persona = {
+    nombre: "Carlos",
+    edad: 25
+}
+
+persona;
