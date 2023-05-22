fb0103689c3afcb1e15c1f2c0d21672fc58fb33b
Carlos Andrés Agudelo <kharlz.agudelo@hotmail.com>
2023-05-22 18:50:30 -0500

    Creando archivo de clase de if, else and else if

---
+++ if.js
@@ -0,0 +1,9 @@
+var edad = 18;
+
+if (edad===18) {
+    console.log("Puedes votar, bienvenido a tu 1ra votación");
+} else if (edad>18) {
+    console.log("Puedes votar de nuevo")
+} else {
+    console.log("Tu no puedes votar")
+}
