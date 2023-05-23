de05f1d0360a325aef9f699e64b06b07ee497caf
Carlos Andrés Agudelo <kharlz.agudelo@hotmail.com>
2023-05-22 19:09:37 -0500

    Creando y editando archivo de clase switch

---
+++ switch.js
@@ -0,0 +1,15 @@
+var numero = 1;
+
+switch (numero) {
+    case 1:
+        console.log("Es uno");
+        break;
+    case 2:
+        console.log("Es dos");
+        break;
+    case 3:
+        console.log("Es tres");
+        break;
+    default:
+        console.log("No es un numero valido");
+}
