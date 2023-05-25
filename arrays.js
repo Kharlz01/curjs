6050ce7a8d0557d3385c961df115b5daf6cc32a9
Carlos Andrés Agudelo <kharlz.agudelo@hotmail.com>
2023-05-25 18:55:54 -0500

    Creando y editando archivo de clase arrays

---
+++ arrays.js
@@ -0,0 +1,23 @@
+var frutas = ["Manzana", "Plátano", "Cereza", "Fresa"];
+
+console.log(frutas);
+
+// Agregar elemento a la ultima posición
+
+var masFrutas = frutas.push("Uvas");
+
+// Elimina elementos
+
+var ultimo = frutas.pop("Uvas");
+
+// Agregar un elemento al inicio
+
+var nuevaLongitud = frutas.unshift("Uvas");
+
+// Eliminar el primer elemento
+
+var borrarFruta = frutas.shift("Uvas");
+
+// Determina la posición del elemento
+
+var posicion = frutas.indexOf("Cereza");
