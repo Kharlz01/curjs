5bbc11ceddfb473557500cfba44c7cd4bf98084d
Carlos Andrés Agudelo <kharlz.agudelo@hotmail.com>
2023-05-26 23:04:27 -0500

    Editando archivo de clase de ciclos con while y reto

--- for.js
+++ for.js
@@ -4,6 +4,26 @@ function saludarEstudiantes(estudiante) {
     console.log(`Hola, ${estudiante}`)    
 }
 
+// Estructura For
+
 for(var i = 0; i < estudiantes.length; i++){
     saludarEstudiantes(estudiantes[i]);
+}
+
+// Estructura While
+
+while(estudiantes.length > 0){
+    var estudiante = estudiantes.shift();
+    saludarEstudiantes(estudiante);
+}
+
+// Reto
+
+function solution(estudiantes, deathCount, nuevo) {
+    while (deathCount > 0) {
+      estudiantes.pop();
+      deathCount -= 1;
+    }
+    estudiantes.push(nuevo)
+    return estudiantes;
 }
