2998bd14b787566ed6dc5453a9403a2ace01ae1c
Carlos Andrés Agudelo <kharlz.agudelo@hotmail.com>
2023-05-28 12:09:13 -0500

    Editando archivo de clase de objetcs con funcion constructora

--- objetcs.js
+++ objetcs.js
@@ -3,5 +3,18 @@
 var miAuto = {
     marca: "Toyota",
     modelo: "Corolla",
-    annio: 2020
+    annio: 2020,
+    detalles: function(){
+        console.log(`Auto ${this.modelo} ${this.annio}`);
+    }
 };
+
+// Funcion Constructora
+
+function auto(marca,modelo,annio){
+    this.marca = marca;
+    this.modelo = modelo;
+    this.annio = annio;
+}
+
+var autoNuevo = new auto("Tesla","Model 3",2020);
